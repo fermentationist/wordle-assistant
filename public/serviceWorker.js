6ab42cde7807351ece8924c5d0abec0b28f4afbd
Dennis Hodges <fermentationist@gmail.com>
2022-12-01 13:57:06 -0600

    Added manifest.json and service worker to make into PWA

---
+++ public/serviceWorker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "wordle_assistant";
+
+const preCache = async resources => {
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    return cache.addAll(resources);
+  } catch (error) {
+    console.error("error loading precache:", error);
+  }
+};
+
+const handleFetch = async event => {
+  const {request} = event;
+  if (request.method.toLowerCase() === "get") {
+    console.log("request:", request);
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(request);
+    let response = cached;
+    if (!cached) {
+      response = await fetch(request).catch(error => error);
+      const responseClone = response.clone();
+      await cache.add(request, responseClone).catch(error => {/*do nothing*/});
+      console.log("response:", response);
+    } else {
+      console.log("cached response:", cached);
+    }
+    return response;
+  }
+}
+
+self.addEventListener("install", event => {
+  console.log("service worker installing...");
+  event.waitUntil(preCache(["/icons/favicon.ico", "/assets"]));
+});
+
+self.addEventListener("activate", event => {
+  const serviceWorker = event.target.serviceWorker;
+  console.log("service worker being activated:", serviceWorker);
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(handleFetch(event));
+})
---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,27 @@
+const registerServiceWorker = async () => {
+  console.log("env:", process.env.NODE_ENV)
+  if ("serviceWorker" in navigator && process.env.NODE_ENV === "production") {
+    try {
+      const registration = navigator.serviceWorker.register("./serviceWorker.js");
+      if (registration.installing) {
+        console.log("service worker installing");
+      } else if (registration.waiting) {
+        console.log("service worker installed and waiting");
+      } else if (registration.active) {
+        console.log("service worker active");
+      }
+    } catch (error) {
+      console.error("service worker registration failed with error:", error);
+    }
+  }
+}
+
+export const unregister = async () => {
+  console.log("unregister called. unregistering all service workers")
+  const registrations = await navigator.serviceWorker.getRegistrations();
+  for (const registration of registrations) {
+    await registration.unregister();
+  }
+}
+
+export default registerServiceWorker;
